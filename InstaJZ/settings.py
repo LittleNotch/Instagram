3ccba0b413eee864fc15c4a511fd346fcc1d1bb8
wenboshi <wenboshi.shirley@gmail.com>
2020-06-30 12:56:24 -0700

    Prep for Deployment

--- InstaJZ/settings.py
+++ InstaJZ/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#4d_bq_^&tv(2uri*-_2k*yp^x^(&qwh#cpqkh_h!3k3^d_c9d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'Insta',
     'imagekit',
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
