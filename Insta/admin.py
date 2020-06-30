44ade5b5d977e38ef87d13268347da35fb487273
wenboshi <wenboshi.shirley@gmail.com>
2020-06-29 19:50:20 -0700

    dislay author of a post, the count of likes

--- Insta/admin.py
+++ Insta/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
-from Insta.models import Post, PostTwo, InstaUser
+from Insta.models import Post, InstaUser, Like
 
 # Register your models here.
 admin.site.register(Post)
-admin.site.register(PostTwo)
 admin.site.register(InstaUser)
+admin.site.register(Like)
